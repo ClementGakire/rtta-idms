0ca1e44b6281e8824219948f8f75f5c53dc5aff2
Clement Gakire <clementgakire@gmail.com>
2025-08-27 21:08:57 +0200

    user sessions

--- vavarw/resources/views/users/sessions/index.blade.php
+++ vavarw/resources/views/users/sessions/index.blade.php
@@ -39,9 +39,7 @@
 </div>
 @endsection
 
-    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/buttons/1.5.6/js/dataTables.buttons.min.js"></script>
     <script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.flash.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
@@ -51,48 +49,45 @@
     <script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.print.min.js"></script>
     <script>
       $(document).ready(function(){
-        $.fn.dataTable.ext.search.push(
-          function (settings, data, dataIndex) {
-              var min = $('#min').datepicker("getDate");
-              var max = $('#max').datepicker("getDate");
-              var startDate = new Date(data[0]);
-              if (min == null && max == null) { return true; }
-              if (min == null && startDate <= max) { return true;}
-              if(max == null && startDate >= min) {return true;}
-              if (startDate <= max && startDate >= min) { return true; }
-              return false;
+        // initialize jQuery UI datepickers (if available)
+        try {
+          if ($.fn.datepicker) {
+            $("#min").datepicker({ changeMonth: true, changeYear: true, dateFormat: 'yy-mm-dd' });
+            $("#max").datepicker({ changeMonth: true, changeYear: true, dateFormat: 'yy-mm-dd' });
           }
-        );
+        } catch (e) {
+          console && console.warn && console.warn('Datepicker init failed', e);
+        }
 
-        $("#min").datepicker({ 
-          onSelect: function () { table.draw(); }, 
-          changeMonth: true, 
-          changeYear: true,
-          dateFormat: 'yy-mm-dd'
+        // Custom date-range filter using string parsing (ISO YYYY-MM-DD)
+        $.fn.dataTable.ext.search.push(function (settings, data, dataIndex) {
+          try {
+            var minVal = $('#min').val();
+            var maxVal = $('#max').val();
+            var dateStr = data[0];
+            if (!dateStr) return true;
+            var rowDate = new Date(dateStr + 'T00:00:00');
+            if (!minVal && !maxVal) return true;
+            if (minVal && !maxVal) {
+              var minDate = new Date(minVal + 'T00:00:00');
+              return rowDate >= minDate;
+            }
+            if (!minVal && maxVal) {
+              var maxDate = new Date(maxVal + 'T23:59:59');
+              return rowDate <= maxDate;
+            }
+            var minDate = new Date(minVal + 'T00:00:00');
+            var maxDate = new Date(maxVal + 'T23:59:59');
+            return rowDate >= minDate && rowDate <= maxDate;
+          } catch (e) {
+            console && console.warn && console.warn('Date filter error', e);
+            return true;
+          }
         });
-        $("#max").datepicker({ 
-          onSelect: function () { table.draw(); }, 
-          changeMonth: true, 
-          changeYear: true,
-          dateFormat: 'yy-mm-dd'
-        });
 
-        // clone header for column search
-        $('#sessions-table thead tr').clone(true).appendTo( '#sessions-table thead' );
-        $('#sessions-table thead tr:eq(0) th').each( function (i) {
-            var title = $(this).text();
-            $(this).html( '<input type="text" style="width:150px;" placeholder=" '+title+'" />' );
-            $( 'input', this ).on( 'keyup change', function () {
-                if ( table.column(i).search() !== this.value ) {
-                    table
-                        .column(i)
-                        .search( this.value )
-                        .draw();
-                }
-            } );
-        } );
-
-        var table = $('#sessions-table').DataTable( {
+        var table = null;
+        try {
+          table = $('#sessions-table').DataTable( {
             orderCellsTop: true,
             fixedHeader: true,
             "order": [[0, "desc"]],
@@ -105,9 +100,29 @@
               { extend: 'csvHtml5', title: 'User Sessions', footer: true, header: true },
               { extend: 'print', footer: true, header: true }
             ]
-        } );
+          } );
 
-        // redraw on date change
-        $('#min, #max').change(function () { table.draw(); });
+          // clone header for column search (after table init)
+          try {
+            $('#sessions-table thead tr').clone(true).appendTo( '#sessions-table thead' );
+            $('#sessions-table thead tr:eq(0) th').each( function (i) {
+                var title = $(this).text();
+                $(this).html( '<input type="text" style="width:150px;" placeholder=" '+title+'" />' );
+                $( 'input', this ).on( 'keyup change', function () {
+                    if ( table.column(i).search() !== this.value ) {
+                        table
+                            .column(i)
+                            .search( this.value )
+                            .draw();
+                    }
+                } );
+            } );
+          } catch (e) { console && console.warn && console.warn('Header clone failed', e); }
+
+          // redraw on date change
+          $('#min, #max').change(function () { if (table) table.draw(); });
+        } catch (e) {
+          console && console.warn && console.warn('DataTable init failed', e);
+        }
       });
     </script>
